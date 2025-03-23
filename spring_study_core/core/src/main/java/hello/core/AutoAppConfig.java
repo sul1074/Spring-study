0b8c59c95f27c9e257d99b510295dcf5f8d9f114
sul1074 <sul107485@gmail.com>
2025-03-23 17:02:50 +0900

    2025.03.23

--- spring_study_core/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring_study_core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
         // AppConfig 등 현재 프로젝트에서는 수동 등록된 정보를 자동으로 등록되게 하면 안되므로 제외
         // 실제로는 같이 사용하기도 함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class),
-        basePackages = "hello.core.member" // 탐색을 시작할 위치 설정
+        basePackages = "hello.core" // 탐색을 시작할 위치 설정
 )
 public class AutoAppConfig {
 }
