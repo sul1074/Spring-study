bfe12335bece54c2c91a48800fffa90c01a32308
sul1074 <sul107485@gmail.com>
2025-11-21 13:12:13 +0900

    feat/페이징과 한계 돌파

--- spring_study_jpa-spring_boot/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ spring_study_jpa-spring_boot/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
