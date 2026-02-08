8696d297f9cf607218bde5e11dedc3423ec54c94
sul1074 <sul107485@gmail.com>
2026-02-08 13:11:05 +0900

    feat/jdbc template

---
+++ spring_study_db/src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,35 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV4 {
+
+    private final MemberRepository repository;
+
+    // 트랜잭션 시작, 커밋, 롤백 코드가 제거됨
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = repository.findById(fromId);
+        Member toMember = repository.findById(toId);
+
+        repository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        repository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
