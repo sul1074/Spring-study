a34121de5b854bfb0ee9b8b83106e2b37a396bec
sul1074 <sul107485@gmail.com>
2026-03-11 23:07:17 +0900

    feat/conveter & formatter

---
+++ spring_study_mvc2/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        log.info("value = {}", intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("value = {}", data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        log.info("ip = {}", ipPort.getIp());
+        log.info("port = {}", ipPort.getPort());
+        return "ok";
+    }
+}
---
+++ spring_study_mvc2/typeconverter/src/test/java/hello/typeconverter/convereter/ConverterTest.java
@@ -0,0 +1,43 @@
+package hello.typeconverter.convereter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("123");
+        assertThat(result).isEqualTo(123);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(123);
+        assertThat(result).isEqualTo("123");
+    }
+
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void IpPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
